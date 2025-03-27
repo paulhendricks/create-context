0856a6577a5e14e0286b642d7634aee21b17d086
Paul Hendricks <phendricks@nvidia.com>
2025-03-27 15:00:29 -0400

    supporting zig.zon

--- src/main.rs
+++ src/main.rs
@@ -53,6 +53,7 @@ fn determine_language(file_path: &str) -> String {
     let extension_to_language: HashMap<&str, &str> = HashMap::from([
         ("rs", "rust"),
         ("zig", "zig"),
+        ("zon", "zig"),
         ("go", "go"),
         ("py", "python"),
         ("cpp", "cpp"),
